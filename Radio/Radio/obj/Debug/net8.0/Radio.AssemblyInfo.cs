7872a5314cfd02ddd5770db49bd4312abcec07f9
Edanker <eledyall@bk.ru>
2024-02-05 16:26:59 +0500

    вщслук

--- Radio/Radio/obj/Debug/net8.0/Radio.AssemblyInfo.cs
+++ Radio/Radio/obj/Debug/net8.0/Radio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Radio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eba2ec7bfa1a31562612ca98e585d4575129f76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+221b9d2ff967ea97576b27358045556977fbbd14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Radio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Radio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
