3dcf618ff43855e21a9ea6b19ce730a5292f9382
Edanker <eledyall@bk.ru>
2024-02-07 22:58:12 +0500

    kestrel

--- src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+667e08c4361ce210c47033059077572eda1a4141")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ab08b1008c1fb447e379588cb36c7cb729cb76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
