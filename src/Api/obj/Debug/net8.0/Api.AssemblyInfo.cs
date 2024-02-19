b7cee4b384454f0a38f3c2df8765da99a61a50a9
Edanker <eledyall@bk.ru>
2024-02-19 17:41:29 +0500

    fix kestrel

--- src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02e71cec0a6d4211e73c0961acbea16ccbf57103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d393d5ad2bd957216151b8aaaa3ead7b41841b1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
