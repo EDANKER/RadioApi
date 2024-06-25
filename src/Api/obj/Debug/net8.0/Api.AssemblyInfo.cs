549bfa879d307a98e91e3948527a976a744cf2e5
Edankryzo <eledyall@bk.ru>
2024-06-25 11:05:23 +0300

    fix mysql

--- src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e403e2d53d57bbd33f74f62562ae6dcfbae1ff4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e504282fde496477084a10f7061c2ed53c80f2fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
